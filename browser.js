3ab0093e405a04ed7e8165929a650d5bd57edf16
Wervice <wervice@proton.me>
2023-10-10 00:20:26 +0200

    1.2 Stable Update

--- browser.js
+++ browser.js
@@ -1,6 +1,34 @@
 const { exec } = require('child_process');
 const { exitCode } = require('process');
-const fs = require("fs")
+const fs = require("fs");
+const { urlToHttpOptions } = require('url');
+const { hostname } = require('os');
+const langCodes = {
+    "Python": "19",
+    "JavaScript": "22",
+    "Java": "23",
+    "C++": "16",
+    "C#": "6",
+    "Ruby": "32",
+    "Swift": "137",
+    "Go": "55",
+    "PHP": "24",
+    "Rust": "147",
+    "Kotlin": "145",
+    "TypeScript": "151",
+    "Perl": "51",
+    "Haskell": "40",
+    "Scala": "47",
+    "Lua": "54",
+    "Objective-C": "21",
+    "Dart": "88",
+    "Elixir": "186",
+    "R": "",
+    "Clojure": "104",
+    "SQL": "37",
+    "Assembly": "344",
+    "COBOL": "89",
+}
 win = nw.Window.get()
 win.width = 700
 win.height = 600
@@ -68,7 +96,33 @@ window.onload = function () {
     document.getElementById("url").addEventListener("blur", function () {
         document.getElementById("url").value = document.getElementById("webview").src.split("//")[1]
     })
+    window.addEventListener("keydown", function (e) {
+        if (e.key == "b" && e.ctrlKey) {
+            showFavs()
+        }
+        else if (e.key == "r" && e.ctrlKey) {
+            document.getElementById("webview").src = curl
+        }
+        else if (e.key == "w" && e.ctrlKey) {
+            exec("start " + curl)
+        }
+        else if (e.key == "q" && e.ctrlKey) {
+            window.close()
+        }
+        else if (e.key == "s" && e.ctrlKey) {
+            launchBugAssistant()
+        }
+    })
+
+    if (
+        localStorage.getItem("browserSetup") == undefined
+    ) {
+        document.getElementById("intro").hidden = false
+    }
+
 }
+
+// !! Window onload end
 
 function openInNewBrowser() {
     exec("start " + document.getElementById("webview").src)
@@ -89,8 +143,7 @@ function addToFavs() {
         }) + curl + ";")
         document.getElementById("bookmarkIcon").src = "images/bookmark.png"
     }
-    showFavs()
-    document.getElementById("favList").hidden = true
+    reloadFav()
 }
 
 function showFavs() {
@@ -100,7 +153,20 @@ function showFavs() {
         "encoding": "utf-8"
     }).split(";")) {
         if (link != "") {
-            document.getElementById("favList").innerHTML += "<button onclick=document.getElementById('webview').src='"+link+"' data-link='"+link+"' oncontextmenu=removeFav('"+link+"') title='"+link.slice(0, 60)+"'>" + link.slice(0,60) + "</button>"
+            url_no_prot = link.slice(0, 30).replace(/(^\w+:|^)\/\//, '')
+            document.getElementById("favList").innerHTML += "<button onclick=document.getElementById('webview').src='" + link + "' data-link='" + link + "' oncontextmenu=removeFav('" + link + "') title='" + link.slice(0, 60) + "'>" + url_no_prot + "</button>"
+        }
+    }
+}
+
+function reloadFav() {
+    document.getElementById("favList").innerHTML = "<small style=color:grey;>Bookmarks</small><br>"
+    for (link of fs.readFileSync("./favs.txt", {
+        "encoding": "utf-8"
+    }).split(";")) {
+        if (link != "") {
+            url_no_prot = link.slice(0, 30).replace(/(^\w+:|^)\/\//, '')
+            document.getElementById("favList").innerHTML += "<button onclick=document.getElementById('webview').src='" + link + "' data-link='" + link + "' oncontextmenu=removeFav('" + link + "') title='" + link.slice(0, 60) + "'>" + url_no_prot + "</button>"
         }
     }
 }
@@ -109,6 +175,69 @@ function removeFav(l) {
     fs.writeFileSync("./favs.txt", fs.readFileSync("./favs.txt", {
         "encoding": "utf-8"
     }).replace(l + ";", ""))
-    showFavs()
-    document.getElementById("favList").hidden = true
+    reloadFav()
 }
+
+function launchBugAssistant() {
+    document.getElementById("bug_assistant").hidden = false
+}
+
+function terminateBugAssistant() {
+    document.getElementById("bug_assistant").hidden = true
+}
+
+function countWordsFastSearch() {
+    if (document.getElementById("information_fast").value.split(" ").length > 15) {
+        res = "<font style=color:red>" + String(document.getElementById("information_fast").value.split(" ").length - 1) + "</font>"
+    }
+    else {
+        res = document.getElementById("information_fast").value.split(" ").length - 1
+    }
+    res = res + "<br>"
+    document.getElementById("wordCounter_fast").innerHTML = res
+}
+
+function startSearchFast() {
+    problemDescription = document.getElementById("information_fast").value
+    languageUsed = document.getElementById("language_fast").value
+    if (languageUsed != "default") {
+        query = problemDescription + " \"" + languageUsed + "\" OR title:" + problemDescription + " OR \":" + problemDescription + "\" " + languageUsed + " OR site:stackoverflow.com " + problemDescription + " " + languageUsed
+        document.getElementById("webview").src = "https://www.google.com/search?q=" + encodeURIComponent(query)
+    }
+    else {
+        languageUsed = ""
+        for (language of ["Python", "JavaScript", "Java", "C++", "C#", "Ruby", "Swift", "Go", "PHP", "Rust", "Kotlin", "TypeScript", "Perl", "Haskell", "Scala", "Lua", "Objective-C", "Dart", "Elixir", "R", "Clojure", "SQL", "Assembly", "COBOL"]) {
+            if (document.getElementById("information_fast").value.toLocaleLowerCase().includes(language.toLocaleLowerCase() + " ") || document.getElementById("information_fast").value.toLocaleLowerCase().includes(" " + language.toLocaleLowerCase())) {
+                if (language.length != 1) {
+                    languageUsed = language
+                }
+            }
+        }
+        query = problemDescription + " \"" + languageUsed + "\" OR title:" + problemDescription + " OR \":" + problemDescription + "\" " + languageUsed + " OR site:stackoverflow.com " + problemDescription + " " + languageUsed
+        document.getElementById("webview").src = "https://www.google.com/search?q=" + encodeURIComponent(query)
+    }
+    terminateBugAssistant()
+}
+
+function startSearchAdvanced() {
+    programmingLanguage = document.getElementById("language_advanced").value
+    programmingLanguageVersion = document.getElementById("language_version_advanced").value
+    errorDescription = document.getElementById("error_description_advanced").value
+    errorDescriptionCode = document.getElementById("error_code_advanced").value
+    frameworkName = document.getElementById("framework_advanced").value
+    query = errorDescription + " \"" + errorDescriptionCode + "\" " + programmingLanguage + " " + programmingLanguageVersion + " " + frameworkName
+    document.getElementById("webview").src = "https://www.google.com/search?q=" + encodeURIComponent(query)
+    terminateBugAssistant()
+}
+
+function setBugFixerMode(mode) {
+    if (mode == "fast") {
+        document.getElementById("search_fast").hidden = false
+        document.getElementById("search_advanced").hidden = true
+    }
+    else if (mode == "advanced") {
+        document.getElementById("search_fast").hidden = true
+        document.getElementById("search_advanced").hidden = false
+    }
+}
+
