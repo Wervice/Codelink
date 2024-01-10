9edfc8d3f18ca915169737a3f7b3bad2fd27d506
Wervice <wervice@proton.me>
2024-01-10 18:28:49 +0100

    Setting up basic html templates

--- zentrox/index.js
+++ zentrox/index.js
@@ -5,40 +5,141 @@ const path = require("path")
 const app = express()
 const os = require("os")
 const fs = require("fs")
+const bodyParser = require('body-parser')
+const crypto = require("crypto")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+
 
 const zentroxInstPath = path.join(os.homedir(), "zentrox/")
+
+if (!fs.existsSync(path.join(zentroxInstPath, "sessionSecret.txt"))) {
+    fs.mkdirSync(zentroxInstPath)
+    fs.writeFileSync(path.join(zentroxInstPath, "sessionSecret.txt"), crypto.randomBytes(64).toString("ascii"))
+}
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(session({
+    secret: fs.readFileSync(path.join(zentroxInstPath, "sessionSecret.txt")).toString("utf8"),
+    name: "sessionMangemenet",
+    cookie: {
+        secure: true,
+        httpOnly: true,
+    }
+}))
 
 // Setup EJS
 app.set('views', __dirname + '/templates');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
+function auth(username, password, req) {
+    users = fs.readFileSync(path.join(zentroxInstPath, "users.txt")).toString().split("\n")
+    for (user of users) {
+        if (Buffer.from(user.split(": ")[0], "utf-8")) {
+            console.log("JEAH")
+        }
+    }
+}
+
 function startsetup() {
     fs.mkdirSync(zentroxInstPath)
     fs.mkdirSync(path.join(zentroxInstPath, "users"))
     fs.writeFileSync(path.join(zentroxInstPath, "zentrox.txt"), "")
+    fs.writeFileSync(path.join(zentroxInstPath, "users.txt"), "")
+    fs.writeFileSync(path.join(zentroxInstPath, ""))
+}
+
+function hash512(str) {
+    var hash = crypto.createHash("sha512")
+    data = hash.update(str, "utf-8")
+    return data.digest("hex")
 }
 
 app.get('/', (req, res) => {
-    res.render(path.join(__dirname, "templates/index.html"))
+    if (!fs.existsSync(path.join(zentroxInstPath, "setupDone.txt"))) {
+        res.render(path.join(__dirname, "templates/index.html"))
+    }
+    else {
+        if (req.session.signedIn != true) {
+            res.render(path.join(__dirname, "templates/welcome.html"), {
+                serverName: fs.readFileSync(path.join(zentroxInstPath, "custom.txt")).toString().split("\n")[0].replace("<", "&lt").replace(">", "&gt"),
+                registrationButton: function () {
+                    if (fs.readFileSync(path.join(zentroxInstPath, "regMode.txt")).toString().split("\n")[0] == "public") {
+                        return "<button class=outline>Sign up</button>"
+                    }
+                    else {
+                        return ""
+                    }
+                }()
+            })
+        }
+        else {
+            res.redirect("/dashboard")
+        }
+    }
 })
 
-/*
-app.get('/', (req, res) => { // URL is /
-    res.render(path.join(__dirname, "templates/index.html") // File is index.html from templates, {
-        t: "a" // Variable t = "a"
-    })
+app.post('/login', (req, res) => {
+    auth(req.body.username, req.body.password, req)
 })
-
-<%= t%> Calls t => = "a"
-*/
 
 app.get('/setup', (req, res) => {
-    res.render(path.join(__dirname, "templates/setup.html"))
+    if (!fs.existsSync(path.join(zentroxInstPath, "setupDone.txt"))) {
+        res.render(path.join(__dirname, "templates/setup.html"))
+    }
+    else {
+        res.redirect("/")
+    }
 })
 
-app.post('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, "templates/dashboard.html"))
+app.post('/setup/registAdmin', (req, res) => {
+    if (fs.existsSync(path.join(zentroxInstPath, "admin.txt"))) {
+        res.status(403).send("This action is not allowed")
+    } else {
+        fs.writeFileSync(path.join(zentroxInstPath, "users.txt"), Buffer.from(req.body.adminUsername, "base64") + ": " + hash512(req.body.adminPassword)+"\n")
+        res.send({
+            "status": "s"
+        })
+    }
+})
+
+app.post('/setup/regMode', (req, res) => {
+    if (fs.existsSync(path.join(zentroxInstPath, "regMode.txt"))) {
+        res.status(403).send("This action is not allowed")
+    } else {
+        fs.writeFileSync(path.join(zentroxInstPath, "regMode.txt"), req.body.regMode)
+        res.send({
+            "status": "s"
+        })
+    }
+})
+
+app.post('/setup/custom', (req, res) => {
+    if (fs.existsSync(path.join(zentroxInstPath, "custom.txt"))) {
+        res.status(403).send("This action is not allowed")
+    } else {
+        fs.writeFileSync(path.join(zentroxInstPath, "custom.txt"), req.body.serverName + "\n" + req.body.cltheme)
+
+        // ? Finish setup process
+        fs.writeFileSync(path.join(zentroxInstPath, "setupDone.txt"), "true")
+        req.session.signedIn = true
+
+        res.send({
+            "status": "s"
+        })
+    }
+})
+
+app.get('/dashboard', (req, res) => {
+    if (req.session.signedIn == true) {
+        res.render(path.join(zentroxInstPath, "dashboard.html"))
+    }
+    else {
+        res.redirect("/")
+    }
 })
 
 app.use(express.static("static"))
@@ -46,9 +147,11 @@ app.use(express.static("static"))
 app.get('/api', (req, res) => {
 
     if (req.query["r"] == "startsetup") {
-        if (!fs.existsSync(zentroxInstPath)) {
+        if (!fs.existsSync(path.join(zentroxInstPath, "setupDone.txt"))) {
             try {
-                startsetup()            
+                if (!fs.existsSync(zentroxInstPath)) {
+                    startsetup()
+                }
                 res.send({
                     "status": "s"
                 })
